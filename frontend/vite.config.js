7db89832e80a688d1dbb1399c13d5aee442726d2
TharunRL <220701302@rajalakshmi.edu.in>
2025-10-10 00:44:28 +0530

    vite config changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        student: resolve(__dirname, 'student.html'),
+
       },
     },
   },
